bf0809a3b3a7cc57883abe63f39f5a434ed10105
Christian Balderrama <0christianbalderrama@gmail.com>
2022-06-22 20:07:11 +0800

    ✨ Add eslint

--- src/index.js
+++ src/index.js
@@ -1,20 +1,20 @@
 async function testAwait() {
   const test = await new Promise((resolve) => {
     setTimeout(() => {
-      resolve(console.log("This is an example async/await and Promises which means this boilerplate handles it as well"));
+      resolve(console.log('This is an example async/await and Promises which means this boilerplate handles it as well')); //eslint-disable-line
     }, 1000);
-  })
+  });
 
   return test;
 }
 
-console.log("Hello, World!");
-
 (() => {
-  console.log("I will log first");
+  console.log('I will log first'); //eslint-disable-line
 })();
 
-(async() => {
+let x = '';
+
+(async () => {
   await testAwait();
-  console.log("HELLo");
+  console.log('HELLo'); //eslint-disable-line
 })();
